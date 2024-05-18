e626e16e84146cd85785a5204460e670f5526e41
Vfivew <hitori555111555@yahoo.com>
2024-05-18 19:04:50 +0300

    fix(frontend):trash tm-3

--- apps/backend/src/libs/modules/config/libs/types/environment-schema.type.ts
+++ apps/backend/src/libs/modules/config/libs/types/environment-schema.type.ts
@@ -7,46 +7,11 @@ type EnvironmentSchema = {
 		HOST: string;
 		PORT: number;
 	};
-	AWS: {
-		ACCESS_KEY: string;
-		REGION: string;
-		S3_BUCKET: string;
-		SECRET_KEY: string;
-		SES_SENDER: string;
-	};
 	DB: {
 		CONNECTION_STRING: string;
 		DIALECT: string;
 		POOL_MAX: number;
 		POOL_MIN: number;
-	};
-	EDX: {
-		URL: string;
-	};
-	JWT: {
-		ALGORITHM: string;
-		EXPIRES_IN: string;
-		SECRET: string;
-	};
-	OPENAI: {
-		API_KEY: string;
-		MODEL: string;
-	};
-	STRIPE: {
-		SECRET_KEY: string;
-	};
-	UDEMY: {
-		CLIENT_ID: string;
-		CLIENT_SECRET: string;
-		URL: string;
-	};
-	UPDATE_PASSWORD: {
-		JWT: {
-			ALGORITHM: string;
-			EXPIRES_IN: string;
-			SECRET: string;
-		};
-		LINK: string;
 	};
 };
 
